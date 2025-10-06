7cc738cabd14408cde679780ade4c87927264d35
namuan <575441+namuan@users.noreply.github.com>
2025-10-06 18:41:06 +0100

    feat(modes): Re-arrange chat modes

--- snap_assist/chat_modes.py
+++ snap_assist/chat_modes.py
@@ -1,12 +1,12 @@
 def get_chat_modes():
     """Returns a dictionary of chat modes."""
     return {
-        "Rewrite": "You are a writing assistant. Rewrite the text provided by the user to improve phrasing. Output ONLY the rewritten text without additional comments. Respond in the same language as the input (e.g., English US, French):",
         "Proofread": "Proofread the text to ensure it uses appropriate grammar, vocabulary, wording, and punctuation. You should maintain the original tone of the text, without altering it to be more formal or less formal. Output ONLY the corrected text without any additional comments. Maintain the original text structure and writing style. Respond in the same language as the input (e.g., English US, French):",
-        "Summarise": "Provide summary in bullet points for the following text:",
-        "Explain": "Can you explain the following:",
+        "Concise": "You are a writing assistant. Rewrite the text provided by the user to be slightly more concise in tone, thus making it just a bit shorter. Do not change the text too much or be too reductive. Output ONLY the concise version without additional comments. Respond in the same language as the input (e.g., English US, French):",
         "Professional": "You are a writing assistant. Rewrite the text provided by the user to sound more professional. Output ONLY the professional text without additional comments. Respond in the same language as the input (e.g., English US, French):",
         "Friendly": "You are a writing assistant. Rewrite the text provided by the user to be more friendly. Output ONLY the friendly text without additional comments. Respond in the same language as the input (e.g., English US, French):",
-        "Concise": "You are a writing assistant. Rewrite the text provided by the user to be slightly more concise in tone, thus making it just a bit shorter. Do not change the text too much or be too reductive. Output ONLY the concise version without additional comments. Respond in the same language as the input (e.g., English US, French):",
+        "Rewrite": "You are a writing assistant. Rewrite the text provided by the user to improve phrasing. Output ONLY the rewritten text without additional comments. Respond in the same language as the input (e.g., English US, French):",
+        "Summarise": "Provide summary in bullet points for the following text:",
+        "Explain": "Can you explain the following:",
         "Fallacy Finder": "I want you to act as a fallacy finder. You will be on the lookout for invalid arguments so you can call out any logical errors or inconsistencies that may be present in statements and discourse. Your job is to provide evidence-based feedback and point out any fallacies, faulty reasoning, false assumptions, or incorrect conclusions which may have been overlooked by the speaker or writer. Text:",
     }
